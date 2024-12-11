c8553715a6e70e11b06d212ef9af0606159f88c4
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-11 20:37:27 +0200

    feat: test function to use dotenv and .env file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,30 @@
-
 plugins {
     kotlin("jvm") version "1.9.24"
     application
 }
 
-
 repositories {
     mavenCentral()
+    maven("https://jitpack.io") // Added Jitpack.io repository
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.0")
-    implementation("io.ktor:ktor-server-netty:2.3.0")
-    implementation("io.ktor:ktor-client-core:2.3.0")
-    implementation("io.ktor:ktor-client-cio:2.3.0")
-    implementation("io.ktor:ktor-client-serialization:2.3.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
+}
+
+sourceSets {
+    main {
+        kotlin.srcDirs("src/main/kotlin")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21)) // Ensure Java 17 or 21 is properly set
+    }
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("com.Event42Sync.MainKt")
 }
-
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,11 @@
-package org.example
+import io.github.cdimascio.dotenv.dotenv
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // Load .env file
+    val dotenv = dotenv()
+    val clientId = dotenv["UID"] ?: error("UID is missing in .env file")
+    val clientSecret = dotenv["SECRET"] ?: error("SECRET is missing in .env file")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("UID: $clientId")
+    println("SECRET $clientSecret")
 }
