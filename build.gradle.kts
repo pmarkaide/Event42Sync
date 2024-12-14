3a83817db20d8683102b4b750f3ca13092c2a8eb
Pablo Markaide <pablo.markaide@gmail.com>
2024-12-14 20:28:35 +0200

    chore: clean gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,24 +5,10 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io") // Added Jitpack.io repository
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.2")
-}
-
-sourceSets {
-    main {
-        kotlin.srcDirs("src/main/kotlin")
-    }
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21)) // Ensure Java 17 or 21 is properly set
-    }
 }
 
 application {
